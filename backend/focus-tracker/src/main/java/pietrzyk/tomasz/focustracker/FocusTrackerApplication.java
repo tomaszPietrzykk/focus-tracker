a647867a86495e9dab5dd69c95ed463a836c2224
tomaszpietrzykk <pietrzyktomek@gmail.com>
2025-05-27 21:15:15 +0200

    Added spring boot project to backend folder

---
+++ backend/focus-tracker/src/main/java/pietrzyk/tomasz/focustracker/FocusTrackerApplication.java
@@ -0,0 +1,13 @@
+package pietrzyk.tomasz.focustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusTrackerApplication.class, args);
+    }
+
+}
---
+++ backend/focus-tracker/src/test/java/pietrzyk/tomasz/focustracker/FocusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pietrzyk.tomasz.focustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
